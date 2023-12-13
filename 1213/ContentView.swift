85b3a30bfb2e54d1246df6712156c8f231c6bc26
Evren2004 <1229zirong@gmail.com>
2023-12-13 20:13:14 +0800

    Initial Commit

---
+++ 1213/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  1213
+//
+//  Created by 訪客使用者 on 2023/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 1213/_213App.swift
@@ -0,0 +1,17 @@
+//
+//  _213App.swift
+//  1213
+//
+//  Created by 訪客使用者 on 2023/12/13.
+//
+
+import SwiftUI
+
+@main
+struct _213App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
